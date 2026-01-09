b5920df50c68df4760b5fa6771579ffd19452da9
Kiran Kasana <kkasanacoder@gmail.com>
2026-01-09 21:29:44 +0530

    Fix firebase import path for Vercel build

--- Frontend/src/pages/LoginSignup.jsx
+++ Frontend/src/pages/LoginSignup.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { auth } from "../firebase";
+import { auth } from "../firebase.js";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import {
