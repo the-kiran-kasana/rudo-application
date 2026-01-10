ae411425c7b9ca015d093d43b654d11b69c91bfc
Kiran Kasana <kkasanacoder@gmail.com>
2026-01-10 09:53:19 +0530

    updsate the again firebase

---
+++ Frontend/src/config/firebase.js
@@ -0,0 +1,35 @@
+//import { initializeApp } from "firebase/app";
+//import { getAuth } from "firebase/auth";
+//
+//const firebaseConfig = {
+//    apiKey: "AIzaSyBqJiG6AzXwp-QwWdLdYwEO5Pq-4SSXC4k",
+//    authDomain: "rudo-application.firebaseapp.com",
+//    projectId: "rudo-application",
+//    storageBucket: "rudo-application.firebasestorage.app",
+//    messagingSenderId: "905346826548",
+//    appId: "1:905346826548:web:77ec589088dfcd87032561",
+//    measurementId: "G-V8JHX7VMY5"
+//};
+//
+//
+//
+//const app = initializeApp(firebaseConfig);
+//export const auth = getAuth(app);
+
+
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
