fe56a770248822873510751e175da44b97808168
Kiran Kasana <kkasanacoder@gmail.com>
2026-01-10 10:10:06 +0530

    updsate the again firebase

--- Frontend/src/pages/LoginSignup.jsx
+++ Frontend/src/pages/LoginSignup.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { auth } from "../config/firebase";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import {
   GoogleAuthProvider,
   signInWithPopup,
